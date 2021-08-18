6dfff03d04382c8dc776616131468ab7caaa35a1
hifinerd <69434858+hifinerd@users.noreply.github.com>
2021-08-18 16:07:51 -0400

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,8 @@
+def my_function(fname):
+  print(str(fname) + " place is " + thisdict[fname] + " and " + str(fname + 1) + " place is " + thisdict[fname + 1])
+thisdict = {
+  1: "Gerald",
+  2: "John",
+  3: "Carl"
+}
+my_function(1)
